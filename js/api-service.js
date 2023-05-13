f903c16446186c98e02b9595abd8666d23b73cbf
Vitalii Kozha <116120008+KozhaVitalii@users.noreply.github.com>
2023-05-13 10:09:52 +0300

    1

--- js/api-service.js
+++ js/api-service.js
@@ -1,3 +1,43 @@
+// 2. Именованный экспорт/импорт
+// Выполняется когда у нас несколько сущностей, которые необходимо импортировать
+
+// const fetchAllUsers = () => {
+//   console.log('fetchAllUsers');
+// };
+
+// const fetchUserById = id => {
+//   console.log('fetchUserById');
+// };
+
+// const updateUserById = id => {
+//   console.log('updateUserById');
+// };
+
+// В итоге можем эти три функции экспортировать по дефолту. Сделать эти 3 функции как свойства этого передаваемого объекта
+
+// export default { fetchAllUsers, fetchUserById, updateUserById };
+
+// Если же мы хотим передать три функции отдельно, то это будет синтаксис именного экспорта:
+
+// export const fetchAllUsers = () => {
+//   console.log('fetchAllUsers');
+// };
+
+// export const fetchUserById = id => {
+//   console.log('fetchUserById');
+// };
+
+// export const updateUserById = id => {
+//   console.log('updateUserById');
+// };
+
+// export const x = 5;
+
+// export const y = 'Myron';
+
+
+// Подключение внешней библиотеки shortid:
+
 import shortid from 'shortid';
 
 export const fetchAllUsers = () => {
@@ -20,3 +60,6 @@ export const addUser = name => {
 
   console.log(user);
 };
+
+// и далее принимаем эту библиотеку в основном файле AudioParamMap.js
+
